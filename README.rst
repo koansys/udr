9f8ce85dd4432a19b19d602921d1d8c383a85260
Reed O'Brien <reed@koansys.com>
2011-01-19 02:27:03 -0500

    initial commit

---
+++ README.rst
@@ -0,0 +1,11 @@
+udr
+================
+
+A Unicode Dict Reader in the spirit of csv.DictReader that recodes
+lines in UTF-8. It mostly extends the csv UnicodeReader from the
+python csv docs and makes it a dict reader.
+
+Thanks go to the authors of csv, csv.DictReader and the csv doc author.
+
+Please see docs/index.rst for the documentation.
+
---
+++ udr/__init__.py
@@ -0,0 +1,86 @@
+import codecs
+import csv
+
+ENCODINGS = ('utf8', 'cp1252', 'latin1', 'ascii', 'utf16')
+
+
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+class UnicodeDictReader:
+    """
+    A CSV Dict reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given or guessed if not given encoding.
+    """
+    def __init__(self, f,
+                 dialect=csv.excel,
+                 encoding=None,
+                 fieldnames=None,
+                 restkey=None,
+                 restval=None,
+                 **kwds):
+        self.encoding = encoding if encoding else guess_encoding(f.read())
+        f.seek(0)
+        f = UTF8Recoder(f, self.encoding)
+        self._fieldnames = fieldnames
+        self.line_num = 0
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+        self.restkey = restkey
+        self.restval = restval
+
+    @property
+    def fieldnames(self):
+        if self._fieldnames is None:
+            try:
+                self._fieldnames = [unicode(s.strip(), "utf-8") for s in self.reader.next()]
+            except StopIteration: # pragma: no cover
+                pass
+            except csv.Error as e:
+                raise InvalidHeaderFields(e)
+        self.line_num = self.reader.line_num
+        return self._fieldnames
+
+    fieldnames = fieldnames #pyflakes ignore redef below
+    @fieldnames.setter
+    def fieldnames(self, value):
+        self._fieldnames = [unicode(s.strip(), "utf-8") for s in value]
+
+    def next(self):
+        if self.line_num == 0:
+            self.fieldnames # remove the first row if fieldnames was given
+        row = [unicode(s, "utf-8") for s in self.reader.next()]
+        self.line_num = self.reader.line_num
+        while row == []:
+            row = [unicode(s, "utf-8") for s in self.reader.next()]
+        d = dict(zip(self.fieldnames, row))
+        lf = len(self.fieldnames)
+        lr = len(row)
+        if lf < lr:
+            d[self.restkey] = row[lf:]
+        elif lf > lr:
+            for key in self.fieldnames[lr:]:
+                d[key] = self.restval
+        return d
+
+    def __iter__(self):
+        return self
+
+
+def guess_encoding(data, encodings=ENCODINGS):
+    for encoding in encodings:
+        try:
+            data.decode(encoding)
+            return encoding
+        except UnicodeDecodeError: # pragma: no cover
+            pass
+    return None # pragma: no cover
